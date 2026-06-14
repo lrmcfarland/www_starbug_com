df6a9f293ac3bfad3d56a98fd32bc3257787d67d
Lincoln McFarland <lrm@starbug.com>
2026-06-14 15:40:04 -0700

    Add holly-dev React service

--- holly/src/App.tsx
+++ holly/src/App.tsx
@@ -1,42 +1,27 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <section id="center">
         <div>
-          <h1>starbug.com</h1>
+          <h1>www.starbug.com</h1>
+        </div>
+      </section>
+
+      <section id="left">
+        <div>
+          <p>
+            This web app built with React and Vite as the frontend, Flask as
+            the backend API, and Nginx as a TLS enabled reverse proxy server.
+            It is deployed as a collection of microservices running in Docker
+            containers.
+          </p>
         </div>
       </section>
 
       <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
       <section id="spacer"></section>
     </>
   )
