d76ccb5325438e64951e8f078a8bdbd7fdf83f7f
Lincoln McFarland <lrm@starbug.com>
2026-06-14 20:51:06 -0700

    TODO: Hardcode AWS allowed hosts

--- holly/vite.config.ts
+++ holly/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['ec2-13-52-213-74.us-west-1.compute.amazonaws.com'],
+  },
 })
