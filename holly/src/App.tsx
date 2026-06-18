eb1b34f4be8f207c6c11c7f8ee0ed2304918df19
Lincoln McFarland <lrm@starbug.com>
2026-06-17 18:54:29 -0700

    Add App.test.ts

--- holly/src/App.tsx
+++ holly/src/App.tsx
@@ -12,9 +12,10 @@ function App() {
       <section id="left">
         <div>
           <p>
-            This web app built with React and Vite as the frontend, Flask as
-            the backend API, and Nginx as a TLS enabled reverse proxy server.
-            It is deployed as a collection of microservices running in Docker
+            This web app is built with React/Vite as the frontend, Flask as
+            the backend API, and Nginx as a TLS enabled, reverse proxy server.
+            It is deployed from a GitHub CI/CD pipeline to an AWS EC2 instance
+            as a collection of microservices running in Docker
             containers.
           </p>
         </div>
