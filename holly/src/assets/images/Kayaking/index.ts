9eaa6d09538213b0baba0b67abd64e0fcbdf6604
Lincoln McFarland <lrm@starbug.com>
2026-07-06 12:41:17 -0700

    Add Kayaking page

---
+++ holly/src/assets/images/Kayaking/index.ts
@@ -0,0 +1,13 @@
+import ender from "./ender.jpg"
+import eskimo_roll from "./eskimo_roll.mp4"
+import grand_canyon_1 from "./grand_canyon_1.jpg"
+import grand_canyon_2 from "./grand_canyon_2.jpg"
+import waterfall from "./waterfall.jpg"
+
+export const images = {
+    ender,
+    eskimo_roll,
+    grand_canyon_1,
+    grand_canyon_2,
+    waterfall
+}
--- holly/src/components/Kayaking.tsx
+++ holly/src/components/Kayaking.tsx
@@ -1,16 +1,90 @@
 import React from "react";
-
 import "../App.css";
-import eskimo_roll from "../assets/images/Kayaking/eskimo_roll.mp4"
+import { images } from "../assets/images/Kayaking";
+
+
+const pictures = [
+  {
+    id: 1,
+    title: "Ender",
+    location: "Tuolumne River, California.",
+    date: "1993 May 20",
+    description: "First enders.",
+    details: [],
+    image: images.ender,
+  },
+  {
+    id: 2,
+    title: "Rainbow Falls",
+    location: "San Joaquin River, California.",
+    date: "1993 May 20",
+    description: "Objects in image are older and wiser than they appear.",
+    details: [],
+    image: images.waterfall,
+  },
+  {
+    id: 3,
+    title: "Grand Canyon 1",
+    location: "Colorado River, Arizona.",
+    date: "1993 September",
+    description: "Grand Canyon trip with the Stanford Kayak Club.",
+    details: ['Go Bears!'],
+    image: images.grand_canyon_1,
+  },
+  {
+    id: 4,
+    title: "Grand Canyon 2",
+    location: "Colorado River, Arizona.",
+    date: "1993 September",
+    description: "Grand Canyon trip with the Stanford Kayak Club.",
+    details: [],
+    image: images.grand_canyon_2,
+  },
+];
 
 export const Kayaking: React.FC = () => {
+
+ const pictureList = pictures.map((picture) => {
+    return (
+      <div key={picture.id} className="starbug-card" >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            gap: "2rem",
+          }}
+        >
+          <div style={{ flex: 1 }}>
+            <h1>{picture.title}</h1>
+            <h2>{picture.location} {picture.date}</h2>
+            <img
+              src={picture.image}
+              alt="Sorry, the picture is missing atm."
+              loading="lazy"
+              style={{
+                width: "100%",
+                height: "auto"
+              }}
+            />
+            <p>{picture.description}</p>
+            <ul>
+                {picture.details.map((detail, id) => (
+                    <li key={id}>{detail}</li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="starbug-div">
       <h1>Kayaking</h1>
       <div className="starbug-card">
-        <h2>Eskimo Roll</h2>
+        <h1>Eskimo Roll</h1>
         <video
-          src={eskimo_roll}
+          src={images.eskimo_roll}
           controls
           playsInline
           width="100%"
@@ -22,6 +96,7 @@ export const Kayaking: React.FC = () => {
           And an update from this millennium.
         </p>
       </div>
+      {pictureList}
     </div>
   );
 };
