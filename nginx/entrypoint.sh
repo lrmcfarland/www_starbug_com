4f02ccefdc964aae5f5d6dbbb647513ed87fa7b5
Lincoln McFarland <lrm@starbug.com>
2026-08-13 18:42:19 -0700

    Set navigator port in an environment variable

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Set default for HOLLY_PORT if not provided
 export HOLLY_PORT="${HOLLY_PORT:-3000}"
 
+# Set default for NAVIGATOR_PORT if not provided
+export NAVIGATOR_PORT="${NAVIGATOR_PORT:-5000}"
+
 # Determine which template to use based on environment or default to production
 TEMPLATE_TYPE="${NGINX_TEMPLATE_TYPE:-production}"
 
@@ -20,7 +23,7 @@ case "$TEMPLATE_TYPE" in
 esac
 
 # Substitute environment variables in the selected template
-envsubst '${HOLLY_PORT}' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
+envsubst '${HOLLY_PORT},${NAVIGATOR_PORT}' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g "daemon off;"
